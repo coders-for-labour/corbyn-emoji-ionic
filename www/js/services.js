8d774309ab490644768f8a1a1cb185002431e302
Tom H <tom@codersforcorbyn.com>
2016-09-06 23:42:24 +0100

    Added new images.

--- www/js/services.js
+++ www/js/services.js
@@ -36,6 +36,65 @@ angular.module('corbynemoji.services', [])
     name: 'thumbs',
     defaultText: "#JeremyForLabour",
     description: ""
+  },{
+    category: 'jeremy',
+    name: 'closed_fist',
+    defaultText: "#JeremyForLabour",
+    description: ""
+  },{
+    category: 'jeremy',
+    name: 'wave',
+    defaultText: "#JeremyForLabour",
+    description: ""
+  },{
+    category: 'jeremy',
+    name: 'obi_smile',
+    defaultText: "#JeremyForLabour",
+    description: ""
+  },{
+    category: 'jeremy',
+    name: 'obi_look',
+    defaultText: "#JeremyForLabour",
+    description: ""
+  },{
+    category: 'jeremy',
+    name: 'obi_look_ls',
+    defaultText: "#JeremyForLabour",
+    description: ""
+  },{
+    category: 'jeremy',
+    name: 'obi_closed_fist_ls',
+    defaultText: "#JeremyForLabour",
+    description: ""
+  },{
+    category: 'jeremy',
+    name: 'heart_smile',
+    defaultText: "#JeremyForLabour",
+    description: ""
+  },
+  {
+    category: 'jeremy',
+    name: 'heart_smile_cap',
+    defaultText: "#JeremyForLabour",
+    description: ""
+  },
+  {
+    category: 'jeremy',
+    name: 'heart_wave',
+    defaultText: "#JeremyForLabour",
+    description: ""
+  },
+  {
+    category: 'jeremy',
+    name: 'heart_single_thumb',
+    defaultText: "#JeremyForLabour",
+    description: ""
+  },
+  {
+    category: 'jeremy',
+    name: 'heart_thumbs',
+    defaultText: "#JeremyForLabour",
+    description: ""
   },
     {
       category: 'red-jeremy',
@@ -58,25 +117,70 @@ angular.module('corbynemoji.services', [])
       defaultText: "#JeremyForLabour",
       description: ""
     }, {
+      category: 'red-jeremy',
+      name: 'look_cap',
+      defaultText: "#JeremyForLabour",
+      description: ""
+    }, {
+      category: 'red-jeremy',
+      name: 'smile_no_glasses',
+      defaultText: "#JeremyForLabour",
+      description: ""
+    }, {
+      category: 'red-jeremy',
+      name: 'cartoon_smile',
+      defaultText: "#JeremyForLabour",
+      description: ""
+    }, {
+      category: 'red-jeremy',
+      name: 'cartoon_happy',
+      defaultText: "#JeremyForLabour",
+      description: ""
+    }, {
+      category: 'red-jeremy',
+      name: 'cartoon_eyebrow',
+      defaultText: "#JeremyForLabour",
+      description: ""
+    }, {
+      category: 'red-jeremy',
+      name: 'cartoon_sad',
+      defaultText: "#JeremyForLabour",
+      description: ""
+    }, {
+      category: 'red-jeremy',
+      name: 'cartoon_shock',
+      defaultText: "#JeremyForLabour",
+      description: ""
+    }, {
+      category: 'red-jeremy',
+      name: 'cartoon_angry',
+      defaultText: "#JeremyForLabour",
+      description: ""
+    }, {
+      category: 'stickers',
+      name: 'jc4pm',
+      defaultText: '#JeremyForLabour',
+      description: ''
+    }, {
       category: 'stickers',
       name: 'orgreave_a',
       defaultText: "#JeremyForLabour",
-      description: "In 2012, the Orgreave Truth and Justice Campaign (OTJC) was formed to campaign for a public enquiry into the policing of the Battle of Orgreave - a violent confrontation on 18 June 1984 between police and pickets at a British Steel Corporation (BSC) coking plant in Orgreave, South Yorkshire. Following the success of the Hillsborough Justice Campaign, there’s been increasing pressure for a full report into the way the event was handled and the violence and intimidation used against workers. SOME MORE TEXT TO FORCE SCROLLING!"
+      description: "In 2012, the Orgreave Truth and Justice Campaign (OTJC) was formed to campaign for a public enquiry into the policing of the Battle of Orgreave - a violent confrontation on 18 June 1984 between police and pickets at a British Steel Corporation (BSC) coking plant in Orgreave, South Yorkshire. Following the success of the Hillsborough Justice Campaign, there’s been increasing pressure for a full report into the way the event was handled and the violence and intimidation used against workers."
     }, {
       category: 'stickers',
       name: 'orgreave_b',
       defaultText: "#JeremyForLabour",
-      description: ""
+      description: "In 2012, the Orgreave Truth and Justice Campaign (OTJC) was formed to campaign for a public enquiry into the policing of the Battle of Orgreave - a violent confrontation on 18 June 1984 between police and pickets at a British Steel Corporation (BSC) coking plant in Orgreave, South Yorkshire. Following the success of the Hillsborough Justice Campaign, there’s been increasing pressure for a full report into the way the event was handled and the violence and intimidation used against workers."
     }, {
       category: 'stickers',
       name: 'orgreave_c',
       defaultText: "#JeremyForLabour",
-      description: ""
+      description: "In 2012, the Orgreave Truth and Justice Campaign (OTJC) was formed to campaign for a public enquiry into the policing of the Battle of Orgreave - a violent confrontation on 18 June 1984 between police and pickets at a British Steel Corporation (BSC) coking plant in Orgreave, South Yorkshire. Following the success of the Hillsborough Justice Campaign, there’s been increasing pressure for a full report into the way the event was handled and the violence and intimidation used against workers."
     }, {
       category: 'stickers',
       name: 'orgreave_d',
       defaultText: "#JeremyForLabour",
-      description: ""
+      description: "In 2012, the Orgreave Truth and Justice Campaign (OTJC) was formed to campaign for a public enquiry into the policing of the Battle of Orgreave - a violent confrontation on 18 June 1984 between police and pickets at a British Steel Corporation (BSC) coking plant in Orgreave, South Yorkshire. Following the success of the Hillsborough Justice Campaign, there’s been increasing pressure for a full report into the way the event was handled and the violence and intimidation used against workers."
     }, {
       category: 'stickers',
       name: 'miners_solidarity',
@@ -108,7 +212,42 @@ angular.module('corbynemoji.services', [])
       name: 'rather_fight_tories_b',
       defaultText: "#JeremyForLabour",
       description: ""
-    }
+    },{
+      category: 'stickers',
+      name: 'blacklist_white_bg',
+      defaultText: '#JeremyForLabour',
+      description: ''
+    },{
+      category: 'stickers',
+      name: 'blacklist_black_bg',
+      defaultText: '#JeremyForLabour',
+      description: ''
+    },{
+      category: 'stickers',
+      name: 'blacklist_no_public_contracts',
+      defaultText: '#JeremyForLabour',
+      description: ''
+    },{
+      category: 'stickers',
+      name: 'blacklist_secret_war',
+      defaultText: '#JeremyForLabour',
+      description: ''
+    },{
+      category: 'stickers',
+      name: 'liberty',
+      defaultText: '#JeremyForLabour',
+      description: ''
+    },{
+      category: 'stickers',
+      name: 'you_know',
+      defaultText: '#JeremyForLabour',
+      description: ''
+    },{
+      category: 'stickers',
+      name: 'momentum',
+      defaultText: '#JeremyForLabour',
+      description: ''
+    },
   ];
 
   return {
