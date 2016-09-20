0d02cd92a130d6b0b9172a12afecac62c07c105c
Tom H <tom@codersforcorbyn.com>
2016-09-20 10:39:39 +0100

    Switched feed to extract URL from excerpt
    Actual URLs from the feed don't work - so now we strip the URL from the
    excerpt. This could get a bit flaky so we need to sort out wp-pipes or
    some alternative

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,170 +1,189 @@
 angular.module('corbynemoji.controllers', ['ionic.native'])
-.controller('Home', function($scope, $http, $ionicModal, $cordovaGoogleAnalytics, $ionicPlatform){
+  .controller('Home', function($scope, $http, $ionicModal,
+    $cordovaGoogleAnalytics, $ionicPlatform) {
 
-  $ionicModal.fromTemplateUrl('templates/help-modal.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function(modal){
-    $scope.modal = modal;
-  });
+    $ionicModal.fromTemplateUrl('templates/help-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
 
-  function getLatest(){
-    $http.get("http://jeremoji.codersforcorbyn.com/", { params: {json: 1, count: 10 }  })
-      .success(function(data){
-        $scope.feed = data;
-      })
-      .error(function(data){
-        $scope.feed = {
-          error: "Could not retrieve feed data."
-        };
-        console.log("ERROR: " + data);
-      })
-      .finally(function(){
-        $scope.$broadcast("scroll.refreshComplete");
-      });
-  }
+    function getLatest() {
+      $http.get("http://jeremoji.codersforcorbyn.com/", {
+          params: {
+            json: 1,
+            count: 10
+          }
+        })
+        .success(function(data) {
+          $scope.feed = data;
+        })
+        .error(function(data) {
+          $scope.feed = {
+            error: "Could not retrieve feed data."
+          };
+          console.log("ERROR: " + data);
+        })
+        .finally(function() {
+          $scope.$broadcast("scroll.refreshComplete");
+        });
+    }
 
-  $scope.clean = function(text){
-    return decodeURI(text);
-  };
-
-  $scope.init = getLatest;
-
-  $scope.refresh = getLatest;
-
-  function launchPage(post){
-    window.open(post.url, '_system');
-  }
-  $scope.go = launchPage;
-
-  //Launch the campaign donation site
-  function donateClicked(){
-    $cordovaGoogleAnalytics.trackEvent("Campaign", "Donate", "Begin", 1);
-    window.open('https://new-corbynstays.nationbuilder.com/donate', '_system');
-  }
-  $scope.donateClicked = donateClicked;
-  $scope.platform = ionic.Platform.platform();
-  $scope.help = function(){
-    $cordovaGoogleAnalytics.trackEvent("App", "Help", "View", 1);
-    $scope.modal.show();
-  };
-  $scope.closeHelp = function(){
-    $cordovaGoogleAnalytics.trackEvent("App", "Help", "Dismiss", 1);
-    $scope.modal.hide();
-  };
-  $scope.$on('$destroy', function(){
-    $scope.modal.remove();
-  });
-})
-.controller('EmojiCtrl', function($scope, $stateParams, Emoji, $cordovaSocialSharing, $cordovaFile, $ionicModal, $cordovaGoogleAnalytics, $ionicPlatform) {
-  //Base image path
-  var imgPath = "www/img/emoji/";
-
-
-  $ionicPlatform.ready(function(){
-    $cordovaGoogleAnalytics.trackView('Emoji Category: ' + $stateParams.category);
-  });
-
-  $ionicModal.fromTemplateUrl('templates/help-modal.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function(modal){
-    $scope.modal = modal;
-  });
-
-  $ionicModal.fromTemplateUrl('templates/share-modal.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function(modal){
-    $scope.shareModal = modal;
-  });
-
-  function copy(e){
-
-  }
-
-  //Launch the share process for an emoji
-  function share(shareObj){
-    var options = {
-      message: shareObj.text,
-      files: [imgPath + shareObj.emoji.category + "/" + shareObj.emoji.name + ".png"]
+    $scope.clean = function(text) {
+      return decodeURI(text);
     };
 
-    /**
-    * Success callback, where result contains information about share action
-    **/
-    var shareSuccess = function(result){
-      /* Refer to docs for cordovaSocialShare - result.completed will often return false on android so is
-      *  largely useless for our purposes. It's probably safe to assume that if this callback is called,
-      *  then sharing was successful.*/
+    $scope.init = getLatest;
 
+    $scope.refresh = getLatest;
 
-      //If result.app is populated, then track it
-      if(result.app && result.app !== null && result.app !== ""){
-        $cordovaGoogleAnalytics.trackEvent("Emoji", "Shared To", result.app, 1);
-      }
+    var tagRegex = /(<([^>]+)>)/ig;
 
-      $cordovaGoogleAnalytics.trackEvent("Emoji", "Shared Image", shareObj.emoji.category + "_" + shareObj.emoji.name, 1);
+    function launchPage(post) {
+      window.open(post.excerpt.replace(tagRegex, ""), '_system');
+    }
+
+    $scope.go = launchPage;
+
+    //Launch the campaign donation site
+    function donateClicked() {
+      $cordovaGoogleAnalytics.trackEvent("Campaign", "Donate", "Begin", 1);
+      window.open('https://new-corbynstays.nationbuilder.com/donate',
+        '_system');
+    }
+    $scope.donateClicked = donateClicked;
+    $scope.platform = ionic.Platform.platform();
+    $scope.help = function() {
+      $cordovaGoogleAnalytics.trackEvent("App", "Help", "View", 1);
+      $scope.modal.show();
     };
-
-    /**
-    * Share Error callback, where result contains information about share error
-    */
-    var shareError = function(result){
-
+    $scope.closeHelp = function() {
+      $cordovaGoogleAnalytics.trackEvent("App", "Help", "Dismiss", 1);
+      $scope.modal.hide();
     };
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+    });
+  })
+  .controller('EmojiCtrl', function($scope, $stateParams, Emoji,
+    $cordovaSocialSharing, $cordovaFile, $ionicModal, $cordovaGoogleAnalytics,
+    $ionicPlatform) {
+    //Base image path
+    var imgPath = "www/img/emoji/";
 
-    $cordovaSocialSharing.shareWithOptions(options, shareSuccess, shareError);
-  }
 
-  //Retrieve the img URL for the given emoji
-  function getPath(e){
-    return "img/emoji/" + e.category + "/" + e.name + ".png";
-  }
+    $ionicPlatform.ready(function() {
+      $cordovaGoogleAnalytics.trackView('Emoji Category: ' + $stateParams
+        .category);
+    });
 
-  $scope.category = $stateParams.category;
-  $scope.emoji = Emoji.getCategory($stateParams.category);
-  $scope.startShare = function(e){
+    $ionicModal.fromTemplateUrl('templates/help-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+
+    $ionicModal.fromTemplateUrl('templates/share-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.shareModal = modal;
+    });
+
+    function copy(e) {
+
+    }
+
+    //Launch the share process for an emoji
+    function share(shareObj) {
+      var options = {
+        message: shareObj.text,
+        files: [imgPath + shareObj.emoji.category + "/" + shareObj.emoji.name +
+          ".png"
+        ]
+      };
+
+      /**
+       * Success callback, where result contains information about share action
+       **/
+      var shareSuccess = function(result) {
+        /* Refer to docs for cordovaSocialShare - result.completed will often return false on android so is
+         *  largely useless for our purposes. It's probably safe to assume that if this callback is called,
+         *  then sharing was successful.*/
+
+
+        //If result.app is populated, then track it
+        if (result.app && result.app !== null && result.app !== "") {
+          $cordovaGoogleAnalytics.trackEvent("Emoji", "Shared To", result.app,
+            1);
+        }
+
+        $cordovaGoogleAnalytics.trackEvent("Emoji", "Shared Image",
+          shareObj.emoji.category + "_" + shareObj.emoji.name, 1);
+      };
+
+      /**
+       * Share Error callback, where result contains information about share error
+       */
+      var shareError = function(result) {
+
+      };
+
+      $cordovaSocialSharing.shareWithOptions(options, shareSuccess,
+        shareError);
+    }
+
+    //Retrieve the img URL for the given emoji
+    function getPath(e) {
+      return "img/emoji/" + e.category + "/" + e.name + ".png";
+    }
+
+    $scope.category = $stateParams.category;
+    $scope.emoji = Emoji.getCategory($stateParams.category);
+    $scope.startShare = function(e) {
       $scope.shareObj = {
         emoji: e,
         text: e.defaultText,
         description: e.description
       };
       $scope.shareModal.show();
-  };
-  $scope.share = share;
-  $scope.closeShare = function(){
+    };
+    $scope.share = share;
+    $scope.closeShare = function() {
       $scope.shareModal.hide();
-  };
-  $scope.getPath = getPath;
+    };
+    $scope.getPath = getPath;
 
 
-  //Launch the campaign donation site
-  function donateClicked(){
-    $cordovaGoogleAnalytics.trackEvent("Campaign", "Donate", "Begin", 1);
-    window.open('https://new-corbynstays.nationbuilder.com/donate', '_system');
-  }
-  $scope.donateClicked = donateClicked;
-  $scope.platform = ionic.Platform.platform();
-  $scope.help = function(){
-    $cordovaGoogleAnalytics.trackEvent("App", "Help", "View", 1);
-    $scope.modal.show();
-  };
-  $scope.closeHelp = function(){
-    $cordovaGoogleAnalytics.trackEvent("App", "Help", "Dismiss", 1);
-    $scope.modal.hide();
-  };
-  $scope.$on('$destroy', function(){
-    $scope.modal.remove();
+    //Launch the campaign donation site
+    function donateClicked() {
+      $cordovaGoogleAnalytics.trackEvent("Campaign", "Donate", "Begin", 1);
+      window.open('https://new-corbynstays.nationbuilder.com/donate',
+        '_system');
+    }
+    $scope.donateClicked = donateClicked;
+    $scope.platform = ionic.Platform.platform();
+    $scope.help = function() {
+      $cordovaGoogleAnalytics.trackEvent("App", "Help", "View", 1);
+      $scope.modal.show();
+    };
+    $scope.closeHelp = function() {
+      $cordovaGoogleAnalytics.trackEvent("App", "Help", "Dismiss", 1);
+      $scope.modal.hide();
+    };
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+    });
+
+    $scope.sliderOptions = {
+      height: 100,
+      effect: 'flip'
+    };
+
+    $scope.$on("$ionicSlides.sliderInitialized", function(event, data) {
+      $scope.slider = data.slider;
+    });
+
   });
-
-  $scope.sliderOptions = {
-    height: 100,
-    effect: 'flip'
-  };
-
-  $scope.$on("$ionicSlides.sliderInitialized", function(event, data){
-    $scope.slider = data.slider;
-  });
-
-});
